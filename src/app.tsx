6fa6840d54b91784c6b7f0c17c3b517dcd42697c
Eldar Mametov <e.mametov@innopolis.university>
2024-09-24 23:22:50 +0300

    the registration page is not ready

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import pkg from "../package.json";
+import Registration from './pages/RegistrationPage/RegistrationPage';
 
 const BookPage = lazy(() => import('./pages/BookPage/BookPage'))
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <Suspense fallback={<h1>Loading...</h1>}>
       <Routes>
+        <Route path={`/${pkg.name}/registration`} element={<Registration />} />
         <Route path={`/${pkg.name}/:libraryId/:bookId`} element={<BookPage />}/>
       </Routes>
     </Suspense>
