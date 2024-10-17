3a718bbc64b91a757ae438674cf04b171ca6b6dd
Eldar Mametov <e.mametov@innopolis.university>
2024-10-18 01:06:03 +0300

    last today commit

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,12 +3,13 @@ import axios from 'axios';
 import { LogoAndTitle } from "./logo";
 import { BookList } from "./bookList";
 import Welcome from "./Welcome";
-import  SearchBook  from "./SearchBook";
-import { MainContainer, LeftSection, RightSection } from './styles/MainPage';
+import SearchBook from "./SearchBook";
+import { MainContainer, LeftSection, RightSection, CoolButton, MenuIcon } from './styles/MainPage';
 
 const MainPage = () => {
     const [books, setBooks] = useState([]);
     const [initialBooks, setInitialBooks] = useState([]);
+    const [BurgerMenu, setBurgerMenu] = useState(false);
 
     useEffect(() => {
         const fetchInitialBooks = async () => {
@@ -26,6 +27,11 @@ const MainPage = () => {
 
         fetchInitialBooks();
     }, []);
+
+    const toggleMobileMenu = () => {
+        setBurgerMenu(!BurgerMenu);
+    };
+
     return (
         <MainContainer>
             <LeftSection>
@@ -33,9 +39,16 @@ const MainPage = () => {
                 <SearchBook setBooks={setBooks} initialBooks={initialBooks} />
                 <BookList initialBooks={books} />
             </LeftSection>
-            <RightSection>
+            <RightSection BurgerMenu={BurgerMenu}>
                 <LogoAndTitle />
             </RightSection>
+            <CoolButton onClick={toggleMobileMenu}>
+                <MenuIcon isOpen={BurgerMenu}>
+                    <span />
+                    <span />
+                    <span />
+                </MenuIcon>
+            </CoolButton>
         </MainContainer>
     );
 };
--- src/pages/MainPage/styles/MainPage.ts
+++ src/pages/MainPage/styles/MainPage.ts
@@ -6,15 +6,95 @@ export const MainContainer = styled.div`
     background-color: black;
 `;
 
-export const LeftSection = styled.div`
+export const LeftSection = styled.div<{ BurgerMenu: boolean }>` // Add prop here
     display: flex;
     flex-direction: column;
-    width: 80%;
+    width: ${({ BurgerMenu }) => BurgerMenu ? 'calc(100% - 20px)' : 'calc(100% - 20px)'}; 
+
 `;
 
-export const RightSection = styled.div`
+export const RightSection = styled.div<{ BurgerMenu: boolean }>`
     display: flex;
     flex-direction: column;
-    width: 20%;
+    width: ${({ BurgerMenu }) => BurgerMenu ? '40%' : '20%'}; 
+    transition: transform 0.3s ease-in-out, width 0.3s ease-in-out;
+    transform: ${({ BurgerMenu }) => BurgerMenu ? 'translateX(0)' : 'translateX(100%)'};
+    position: fixed;
+    top: 0;
+    right: 0;
+    height: 100%;
+    background-color: black;
+    z-index: 10;
+    padding: 20px;
 
+    @media (min-width: 1200px) {
+        transform: translateX(0);
+        position: static;
+        background-color: transparent;
+        padding: 0;
+    }
+`;
+
+export const CoolButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 20px; 
+  right: 20px;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  z-index: 11;
+
+  @media (min-width: 1200px) {
+    display: none;
+  }
+`;
+
+
+
+interface MenuIconProps {  
+    isOpen: boolean;
+  }
+
+export const MenuIcon = styled.div<MenuIconProps>`
+  width: 30px;
+  height: 24px;
+  position: relative;
+  cursor: pointer;
+  transform: ${({ isOpen }) => (isOpen ? 'rotate(0deg)' : 'rotate(0deg)')};
+  transition: transform 0.3s ease-in-out;
+
+  span {
+    display: block;
+    position: absolute;
+    height: 3px;
+    width: 100%;
+    background: white;
+    border-radius: 9px;
+    opacity: 1;
+    left: 0;
+    transform: rotate(0deg);
+    transition: 0.25s ease-in-out;
+
+    &:nth-child(1) {
+      top: ${({ isOpen }) => (isOpen ? '10px' : '0px')};
+      transform: ${({ isOpen }) => (isOpen ? 'rotate(135deg)' : 'rotate(0deg)')};
+    }
+
+    &:nth-child(2) {
+      top: 10px;
+      transform: ${({ isOpen }) => (isOpen ? 'translateX(100%)' : 'translateX(0)')};
+      opacity: ${({ isOpen }) => (isOpen ? 0 : 1)};
+    }
+
+    &:nth-child(3) {
+      top: ${({ isOpen }) => (isOpen ? '10px' : '20px')};
+      transform: ${({ isOpen }) => (isOpen ? 'rotate(-135deg)' : 'rotate(0deg)')};
+    }
+  }
 `;
